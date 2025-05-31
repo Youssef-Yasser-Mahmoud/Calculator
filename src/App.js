848b1a7968542fa4578d24f7d7d9468f60cbd891
Youssef <youssefyaser2020@gmail.com>
2025-05-31 17:08:13 +0300

    implement evaluate function for basic arithmetic operations

--- src/App.js
+++ src/App.js
@@ -12,6 +12,29 @@ export const ACTIONS = {
   EVALUATE: 'evaluate',
 };
 
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return '';
+  let computation = '';
+  switch (operation) {
+    case '+':
+      computation = prev + current;
+      break;
+    case '-':
+      computation = prev - current;
+      break;
+    case '*':
+      computation = prev * current;
+      break;
+    case '÷':
+      computation = prev / current;
+      break;
+  }
+
+  return computation.toString();
+}
+
 const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
   maximumFractionDigits: 0,
 });
